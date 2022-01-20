d02c89480ee4fe68aff6eb5e92f31aece89829f9
Veronica <veronicasjoquist@hotmail.com>
2022-01-20 18:35:00 +0100

    Kommit en bit, måste städa upp

--- script.js
+++ script.js
@@ -40,24 +40,56 @@ function finishTodo() {
 }
 
 function LetUserChoose() {
-  userPick = prompt(
-    "Vad vill du göra? \n 0: Avsluta \n 1: Lägga till en ny todo \n 2: Checka av en existerande todo \n 3: Visa alla todos"
-  );
-  user_pick = parseInt(userPick);
-  if (user_pick === 1) {
-    add();
-  } else if (user_pick === 2) {
-    finishTodo();
-  } else if (user_pick === 3) {
-    PrintTodos();
-  } else if (user_pick === 0) {
-    go = false;
-  } else {
-    alert("Please enter a valid option");
+  let go = true;
+  while (go) {
+    userPick = prompt(
+      "Vad vill du göra? \n 0: Avsluta \n 1: Lägga till en ny todo \n 2: Checka av en existerande todo \n 3: Visa alla todos"
+    );
+    user_pick = parseInt(userPick);
+    if (user_pick === 0) {
+      go = false;
+    } else if (user_pick === 1) {
+      add();
+    } else if (user_pick === 2) {
+      finishTodo();
+    } else if (user_pick === 3) {
+      PrintTodos();
+    } else {
+      alert("Please enter a valid option");
+    }
   }
+  printTodos();
 }
 
-let go = true;
-while (go) {
-  LetUserChoose();
+function printTodos() {
+  const ulElement = document.getElementById("todo-list");
+  // generera html kod utifrån vad som finns i todos Arrayen
+  let tasks = "";
+  for (let i = 0; i < todo.length; i++) {
+    const e = todo[i];
+
+    const whenDoneHTML = /*html*/ `
+<span class="badge rounded-pill bg-success">Done</span>
+    `;
+
+    tasks += /*html*/ `
+  <li class="list-group-item">
+    <div class="row justify-content-between m-4">
+        <div class="col">
+          ${e.do}
+        </div>
+        <div class="col">
+          ${e.done ? whenDoneHTML : ""}
+        </div>
+    </div>
+  </li>`;
+  }
+  ulElement.innerHTML = tasks;
 }
+
+printTodos();
+
+const btnElement = document.getElementById("new-todo-button");
+btnElement.onclick = function () {
+  LetUserChoose();
+};
